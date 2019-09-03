887477b6e893b40a673f102a4c56c6e2ab80cc17
Daniela-2017 <51520588+Daniela-2017@users.noreply.github.com>
2019-09-03 01:05:33 -0600

    peticion sincrona-json
    Guardar registros de clientes en un archivo json

---
+++ procesarLogin.php
@@ -0,0 +1,13 @@
+<?php 
+    //cambiar por get
+    if ($_POST!=null){
+    	//obtengo los datos del archivo usuarios.php
+		$contenidoArchivo=file_get_contents('usuarios.json');
+		$usuarios=json_decode($contenidoArchivo,true);
+		$usuarios[] = $_POST;
+
+		$archivo= fopen('usuarios.json', 'w');
+		fwrite($archivo, json_encode($usuarios));
+		fclose($archivo);
+    }
+?>
