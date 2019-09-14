9715a9cef2f664944a3f80a0689820e86dfba743
Daniela-2017 <51520588+Daniela-2017@users.noreply.github.com>
2019-09-13 23:46:57 -0600

    clases php

--- clases/class-cliente.php
+++ clases/class-cliente.php
@@ -1,1 +1,173 @@
+<?php 
 
+    class Usuario{
+        private $nombre;
+        private $apellido;
+        private $pais;
+        private $direccion;
+        private $correo;
+        private $clave;
+        private $confirmacionClave;
+
+        public function __construct(
+            $nombre,
+            $apellido,
+            $pais,
+            $direccion,
+            $correo,
+            $clave,
+            $confirmacionClave
+        ){
+            $this->nombre = $nombre;
+            $this->apellido = $apellido;
+            $this->pais = $pais;
+            $this->direccion = $direccion;
+            $this->correo = $correo;
+            $this->clave = $clave;
+            $this->confirmacionClave=$confirmacionClave;
+        }
+
+        public function getnombre(){
+            return $this->name;
+        }
+
+        public function setnombre($name){
+                $this->name = $name;
+        }
+
+        
+
+        public function getapellido(){
+                return $this->apellido;
+        }
+         
+        public function setapellido($apellido){
+                $this->apellido = $apellido;
+        }
+
+        public function getpais(){
+                return $this->pais;
+        }
+         
+        public function setpais($pais){
+                $this->pais = $pais;
+        }
+
+        public function getdireccion(){
+                return $this->direccion;
+        }
+         
+        public function setdireccion($direccion){
+                $this->direccion = $direccion;
+        }
+
+        public function getcorreo(){
+                return $this->correo;
+        }
+         
+        public function setcorreo($correo){
+                $this->correo = $correo;
+        }
+
+        public function getclave(){
+                return $this->clave;
+        }
+         
+        public function setclave($clave){
+                $this->clave = $clave;
+        }
+
+        public function getconfirmacionClave(){
+            return $this->$confirmacionClave;
+        }
+        public function setconfirmacionClave($confirmacionClave){
+            $this->$confirmacionClave;
+        }
+
+        public function __toString(){
+           return json_encode($this->getData());
+        }
+
+        public function createUser($rutaArchivo,$id){
+
+            $encontrado = false;
+            $contenido = file_get_contents($rutaArchivo);
+            $usuarios = json_decode($contenido,true);
+            for ($i=0; $i < sizeof($usuarios); $i++) { 
+                if($usuarios[$i]['correo']==$id){
+                echo '<script>alert("ya existe)</script>';
+                sleep(2);
+                $encontrado=true;
+            header("Location: Registrar-cliente.html");
+                return;
+            }
+
+            }
+           
+
+            if ($encontrado==false) {
+
+               $usuarios[] = $this->getData();
+                echo '<script>alert("Cliente agregado satisfactoriamente");</script>';
+            $archivo = fopen($rutaArchivo,'w');
+            fwrite($archivo, json_encode($usuarios));
+
+
+            echo json_encode($this->getData());
+            }
+            
+            header("Location: Registrar-cliente.html");
+        }
+
+
+        //Sirve Para Llamar un atributo o metodo de una clase sin instanciar
+        public static function getUsers($rutaArchivo){
+            $contenido = file_get_contents($rutaArchivo);
+            echo $contenido;
+        }
+
+        public static function getUser($rutaArchivo,$id){
+            $contenido = file_get_contents($rutaArchivo);
+            $usuarios = json_decode($contenido, true);
+            if($id>(sizeof($usuarios)-1))
+                echo '{"mensaje":"El Codigo no Existe"}';
+            else
+                echo json_encode($usuarios[$id]);
+        }
+
+        public static function deleteUser($rutaArchivo,$id){
+             $contenido = file_get_contents($rutaArchivo);
+             $usuarios = json_decode($contenido, true);
+             array_splice($usuarios, $id,$id);
+             $archivo = fopen($rutaArchivo, 'w');
+             fwrite($archivo, json_encode($usuarios));
+             fclose($archivo);
+             echo '{"Mensaje":"Se Elimino el Elemento"'.$id.'}';
+        }
+
+        public function updateUser($rutaArchivo,$id){
+             $contenido = file_get_contents($rutaArchivo);
+             $usuarios = json_decode($contenido,true);
+             $usuarios[$id] = $this->getData();
+     
+             $archivo = fopen($rutaArchivo, 'w');
+             fwrite($archivo,json_encode($usuarios));
+             fclose($archivo);
+             echo json_encode($this->getData());
+        }
+
+        //Retorna un arreglo asociativo del a clase
+        public function getData(){
+             $result['nombre'] = $this->nombre;
+             $result['apellido'] = $this->apellido;
+             $result['pais'] = $this->pais;
+             $result['direccion'] = $this->direccion;
+             $result['correo'] = $this->correo;
+             $result['clave'] = $this->clave;
+             $result['confirmacionClave'] = $this->confirmacionClave;
+             return $result;
+        }
+    }
+
+
+?>
--- clases/class-empresa.php
+++ clases/class-empresa.php
@@ -1,1 +1,254 @@
+<?php 
 
+    class Empresa{
+        private $id;
+        private $direccion;
+        private $latitud;
+        private $longitud;
+        private $urlBanner;
+        private $urlLogotipo;
+        private $facebook;
+        private $whatsapp;
+        private $twitter;
+        private $instagram;
+        private $redes;
+        private $correo;
+        private $clave;
+        private $claveConfirmacion;        
+ 
+
+        public function __construct(
+            $id,
+            $direccion,
+            $latitud,
+            $longitud,
+            $urlBanner,
+            $urlLogotipo,
+            $facebook,
+            $whatsapp,
+            $twitter,
+            $instagram,
+            $redes,
+            $correo,
+            $clave,
+            $claveConfirmacion,            
+        ){
+            $this->id = $id;
+            $this->direccion = $direccion;
+            $this->latitud = $latitud;
+            $this->longitud = $longitud;
+            $this->urlBanner = $urlBanner;
+            $this->urlLogotipo = $urlLogotipo;
+            $this->facebook=$facebook;
+            $this->whatsapp=$whatsapp;
+            $this->twitter=$twitter;
+            $this->instagram=$instagram;
+            $this->redes=$redes;
+            $this->correo=$correo;
+            $this->clave=$clave;
+            $this->claveConfirmacion=$claveConfirmacion;            
+        }
+
+        public function getid(){
+            return $this->id;
+        }
+
+        public function setid($id){
+                $this->id = $id;
+        }
+
+        
+
+        public function getdireccion(){
+                return $this->direccion;
+        }
+         
+        public function setdireccion($direccion){
+                $this->direccion = $direccion;
+        }
+
+        public function getlatitud(){
+                return $this->latitud;
+        }
+         
+        public function setlatitud($latitud){
+                $this->latitud = $latitud;
+        }
+
+        public function getlongitud(){
+                return $this->longitud;
+        }
+         
+        public function setlongitud($longitud){
+                $this->longitud = $longitud;
+        }
+
+        public function geturlBanner(){
+                return $this->urlBanner;
+        }
+         
+        public function seturlBanner($urlBanner){
+                $this->urlBanner = $urlBanner;
+        }
+
+        public function geturlLogotipo(){
+                return $this->urlLogotipo;
+        }
+         
+        public function seturlLogotipo($urlLogotipo){
+                $this->urlLogotipo = $urlLogotipo;
+        }
+
+        public function getfacebook(){
+                return $this->facebook;
+        }
+         
+        public function setfacebook($facebook){
+                $this->facebook = $facebook;
+        }
+
+                public function getwhatsapp(){
+                return $this->whatsapp;
+        }
+         
+        public function setwhatsapp($whatsapp){
+                $this->whatsapp = $whatsapp;
+        }
+
+                public function getinstagram(){
+                return $this->instagram;
+        }
+         
+        public function setinstagram($instagram){
+                $this->instagram = $instagram;
+        }
+
+                public function getredes(){
+                return $this->redes;
+        }
+         
+        public function setredes($redes){
+                $this->redes = $redes;
+        }
+
+
+
+
+
+        public function getcorreo(){
+                return $this->correo;
+        }
+         
+        public function setcorreo($correo){
+                $this->correo = $correo;
+        }
+
+        public function getclave(){
+                return $this->clave;
+        }
+         
+        public function setclave($clave){
+                $this->clave = $clave;
+        }
+
+        public function getclaveConfirmacion(){
+            return $this->$claveConfirmacion;
+        }
+        public function setclaveConfirmacion($claveConfirmacion){
+            $this->$claveConfirmacion;
+        }
+
+        public function __toString(){
+           return json_encode($this->getData());
+        }
+
+        public function createEmpresa($rutaArchivo,$id){
+
+            $encontrado = false;
+            $contenido = file_get_contents($rutaArchivo);
+            $empresa = json_decode($contenido,true);
+            for ($i=0; $i < sizeof($empresa); $i++) { 
+                if($empresa[$i]['id']==$id){
+                echo '<script>alert("ya existe)</script>';
+                sleep(2);
+                $encontrado=true;
+            header("Location: Registrar-Empresa.html");
+                return;
+            }
+
+            }
+           
+
+            if ($encontrado==false) {
+
+               $empresa[] = $this->getData();
+                echo '<script>alert("Cliente agregado satisfactoriamente");</script>';
+            $archivo = fopen($rutaArchivo,'w');
+            fwrite($archivo, json_encode($empresa));
+
+
+            echo json_encode($this->getData());
+            }
+            
+            header("Location: Registrar-cliente.html");
+        }
+
+
+        //Sirve Para Llamar un atributo o metodo de una clase sin instanciar
+        public static function getUsers($rutaArchivo){
+            $contenido = file_get_contents($rutaArchivo);
+            echo $contenido;
+        }
+
+        public static function getUser($rutaArchivo,$id){
+            $contenido = file_get_contents($rutaArchivo);
+            $empresa = json_decode($contenido, true);
+            if($id>(sizeof($empresa)-1))
+                echo '{"mensaje":"El Codigo no Existe"}';
+            else
+                echo json_encode($empresa[$id]);
+        }
+
+        public static function deleteEmpresa($rutaArchivo,$id){
+             $contenido = file_get_contents($rutaArchivo);
+             $empresa = json_decode($contenido, true);
+             array_splice($empresa, $id,$id);
+             $archivo = fopen($rutaArchivo, 'w');
+             fwrite($archivo, json_encode($empresa));
+             fclose($archivo);
+             echo '{"Mensaje":"Se Elimino el Elemento"'.$id.'}';
+        }
+
+        public function updateEmpresa($rutaArchivo,$id){
+             $contenido = file_get_contents($rutaArchivo);
+             $empresa = json_decode($contenido,true);
+             $empresa[$id] = $this->getData();
+     
+             $archivo = fopen($rutaArchivo, 'w');
+             fwrite($archivo,json_encode($empresa));
+             fclose($archivo);
+             echo json_encode($this->getData());
+        }
+
+        //Retorna un arreglo asociativo del a clase
+        public function getData(){
+             $result['id'] = $this->id;
+             $result['direccion'] = $this->direccion;
+             $result['latitud'] = $this->latitud;
+             $result['longitud'] = $this->longitud;
+             $result['urlBanner'] = $this->urlBanner;
+             $result['urlLogotipo'] = $this->urlLogotipo;
+             $result['facebook'] = $this->facebook;
+             $result['whatsapp'] = $this->whatsapp;
+             $result['twitter'] = $this->twitter;
+             $result['instagram'] = $this->instagram;
+             $result['redes'] = $this->redes;
+             $result['correo'] = $this->correo;
+             $result['clave'] = $this->clave;
+             $result['claveConfirmacion'] = $this->claveConfirmacion;
+             return $result;
+        }
+    }
+
+
+?>
